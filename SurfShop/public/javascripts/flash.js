4a8b4c7cfc6a224c48ab70c91bcb24e78673e0e1
BonatheRipper <53067387+BonatheRipper@users.noreply.github.com>
2022-04-20 00:57:36 +0100

    Delete SurfShop directory

--- SurfShop/public/javascripts/flash.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function(){
-    if ($('.notify').length > 0) {
-      $('.notify').remove();
-    }
-  }, 5000)
--- SurfShop/public/javascripts/post-index.js
+++
@@ -1,7 +0,0 @@
-const clear = document.getElementById('clear-distance');
-clear.addEventListener('click', (e)=>{
-e.prevenetDefault();
-document.getElementById("location").value = ""
-document.querySelector("input[type=radio]:checked").checked = false;
-
-})
