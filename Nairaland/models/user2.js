1db00caf74f891ef3ac383f111ab68884f623681
BonatheRipper <53067387+BonatheRipper@users.noreply.github.com>
2022-04-20 02:54:25 +0100

    Delete Nairaland directory

--- Nairaland/models/user2.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-const numVal = new Schema({
-    num: Number
-})
-module.exports = mongoose.model("Num", numVal)
--- Nairaland/routes/section.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {viewSection} = require('../controllers/viewSection');
-
-router.get("/", viewSection)
-module.exports =  router;
